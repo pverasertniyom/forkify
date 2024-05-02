40485b11821778bce0a362dbe0eb7dfe15a58591
pverasertniyom <peeradej.v@gmail.com>
2024-05-02 23:56:28 +0700

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'ab3f6776-1192-484a-b646-69657d34a613';
+export const MODAL_CLOSE_SEC = 2.5;
